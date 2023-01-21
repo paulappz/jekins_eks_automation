2eba490c111eac99290fdb84e27642404b6bb0f9
Ubuntu <ubuntu@ip-172-31-2-1.eu-west-2.compute.internal>
2023-01-21 08:32:30 +0000

    netk

--- networking/main.tf
+++ networking/main.tf
@@ -19,7 +19,9 @@ module "vpc" {
   name = local.name
   cidr = "10.0.0.0/16"
 
-  azs             = ["${local.region}d", "${local.region}b", "${local.region}c"]
+#  azs             = ["${local.region}d", "${local.region}b", "${local.region}c"]
+
+  azs             = ["${local.region}b", "${local.region}c"]
   private_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
   public_subnets  = ["10.0.4.0/24", "10.0.5.0/24", "10.0.6.0/24"]
 
